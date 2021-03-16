8c50cb4686c61dbd87a4c8ea8224769c5b808b5f
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-16 14:31:12 -0700

    Next With Apollo

---
+++ apollo-client.js
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemeryCache, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://countries.trevorblades.com",
+    cache: new InMemoryCache(),
+});
+
+export default client;
