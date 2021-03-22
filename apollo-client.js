425fb5f8bcba7e623ee81e3fbfc587c061ca7343
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-03-21 22:37:04 -0400

    Update apollo-client.js

--- apollo-client.js
+++ apollo-client.js
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemeryCache, InMemoryCache } from "@apollo/client";
 
+//Setup apollo client
+
 const client = new ApolloClient({
     uri: "https://countries.trevorblades.com",
     cache: new InMemoryCache(),
